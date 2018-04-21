6f3a9b5f4f9c37299100e04a03c7bbb4b42186b1
Yoon Jo Han <yuniegu3@gmail.com>
2018-04-20 23:43:54 -0400

    Added in some comments showing thought process

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ post '/contact' do
 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 	response = sg.client.mail._('send').post(request_body: mail.to_json)
 	puts response.status_code
-
+	
 	redirect '/'
 end
 
--- public/main.js
+++ public/main.js
@@ -1,15 +1,24 @@
+
+// materialize js 
+
+//for main page slider
+
 $(document).ready(function(){
 $('.slider').slider();
   });
-        
-
-
 $('.slider').slider({full_width: true});//slider init
 
 
-
+//for carousel in the 3 pages cakes,muffins, cookies
 $(document).ready(function(){
 $('.carousel').carousel();
   });
 
 $('.carousel').carousel({full_width: true});
+
+
+
+//for the mobile drop down side nav
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
